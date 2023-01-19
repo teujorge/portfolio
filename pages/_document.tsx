a076d39088bb81d2400e125a3ab35ce91cc5f4e8
Matheus <mrljorge@outlook.com>
2023-01-19 05:02:43 -0300

    move head from document into index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Matheus Jorge's Portfolio</title>
-        <meta name="description" content="Portfolio of Matheus Jorge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,29 +6,39 @@ import { Experience } from "@/components/Experience";
 import { LeftBar } from "@/components/LeftBar";
 import { Projects } from "@/components/Projects";
 import { Title } from "@/components/Title";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main
-      css={css`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+    <>
+      <Head>
+        <title>Matheus Jorge's Portfolio</title>
+        <meta name="description" content="Portfolio of Matheus Jorge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        & div {
-          margin: 40px;
-          max-width: var(--max-width);
-        }
-      `}
-    >
-      <LeftBar />
-      <Title />
-      <About />
-      <Experience />
-      <Projects />
-      <Education />
-      {/* <Contact /> */}
-    </main>
+      <main
+        css={css`
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+
+          & div {
+            margin: 40px;
+            max-width: var(--max-width);
+          }
+        `}
+      >
+        <LeftBar />
+        <Title />
+        <About />
+        <Experience />
+        <Projects />
+        <Education />
+        {/* <Contact /> */}
+      </main>
+    </>
   );
 }
