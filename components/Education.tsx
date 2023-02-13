b95efaa991dd406f01aeccc10f73405dfb490d54
Matheus Jorge <mrljorge@outlook.com>
2023-02-13 13:39:53 -0300

    add flag image of each education location

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,3 +1,7 @@
+import flagMX from "public/images/flags/mx-flag.png";
+import flagUAE from "public/images/flags/uae-flag.png";
+import flagUSA from "public/images/flags/usa-flag.png";
+import Image, { StaticImageData } from "next/image";
 import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
 
@@ -7,27 +11,45 @@ export const Education = () => {
     degree,
     city,
     date,
+    image,
   }: {
     school: string;
     degree: string;
-
     city: string;
     date: string;
+    image: { src: StaticImageData; alt: string };
   }) => {
     return (
       <div
         className="reveal"
         css={css`
           display: flex;
-          flex-direction: column;
+          flex-direction: row;
           justify-content: center;
           align-items: center;
         `}
       >
-        <h4>
-          {degree}, {school}, {city}
-        </h4>
-        <p>{date ? date : "current"}</p>
+        <div
+          css={css`
+            margin-top: 0px !important;
+            margin-bottom: 0px !important;
+            margin-left: 10px !important;
+            margin-right: 10px !important;
+            text-align: center;
+          `}
+        >
+          <h4>
+            {degree}, {school}, {city}
+          </h4>
+          <p>{date ? date : "current"}</p>
+        </div>
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width={25}
+          height={25}
+          quality={100}
+        />
       </div>
     );
   };
@@ -41,6 +63,7 @@ export const Education = () => {
         degree={"BS Mechanical Engineering"}
         city={"Burlington"}
         date={"2020"}
+        image={{ src: flagUSA, alt: "usa-flag" }}
       />
 
       <School
@@ -48,6 +71,7 @@ export const Education = () => {
         degree={"High School"}
         city={"Mexico City"}
         date={"2016"}
+        image={{ src: flagMX, alt: "mx-flag" }}
       />
 
       <School
@@ -55,6 +79,7 @@ export const Education = () => {
         degree={"High School"}
         city={"Abu Dhabi"}
         date={"2015"}
+        image={{ src: flagUAE, alt: "uae-flag" }}
       />
     </div>
   );
