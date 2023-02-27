067dbefe62fc6694b91048120741aa551f6e2ae4
Matheus <mrljorge@outlook.com>
2023-02-27 12:18:51 -0300

    cleanup origin, destination, animation time

--- components/Bubbles.tsx
+++ components/Bubbles.tsx
@@ -21,25 +21,29 @@ type ColorRGBA = {
 const Bubbles = ({
   quantity = 10,
   blur = 100, // px
+  minSpeed = 50, // px/s
+  maxSpeed = 100, // px/s
   minSize = 100, // px
   maxSize = 500, // px
 }: {
   quantity?: number;
   blur?: number;
+  minSpeed?: number;
+  maxSpeed?: number;
   minSize?: number;
   maxSize?: number;
 }) => {
   // a single bubble
   const Bubble = ({ bubbleId }: { bubbleId: string }) => {
     const [origin, setOrigin] = useState<Position>({
-      x: 0,
-      y: -maxSize * 2,
+      x: -maxSize - blur,
+      y: -maxSize - blur,
     });
     const [finalPosition, setFinalPosition] = useState<Position>({
-      x: -maxSize,
-      y: -maxSize,
+      x: -maxSize - blur,
+      y: -maxSize - blur,
     });
-    const [angle, setAngle] = useState<number>(45);
+
     const [size, setSize] = useState<number>(0);
     const [color, setColor] = useState<ColorRGBA>({ r: 0, g: 0, b: 0, a: 0 });
 
@@ -58,31 +62,39 @@ const Bubbles = ({
     function resetBubble() {
       setSize(randSize());
       setColor(randColor());
-      setAngle(randAngle());
-      setOrigin({ ...randOrigin() });
-      setFinalPosition({ ...randFinalPosition() });
+
+      const newOrigin = randOrigin();
+      setOrigin({ ...newOrigin });
+
+      const newFinalPosition = randFinalPosition(newOrigin);
+      setFinalPosition({ ...newFinalPosition });
     }
 
-    // using bottom pos
+    // using top pos
     function randOrigin(): Position {
       return {
-        x: Math.round(Math.random() * window.innerWidth),
-        y: Math.round(Math.random() * -maxSize * 5) - maxSize,
+        x: Math.round(Math.random() * window.innerWidth - maxSize / 2),
+        y: window.innerHeight + blur,
       };
     }
 
     // using translate
-    function randFinalPosition(): Position {
+    function randFinalPosition(origin: Position): Position {
       return {
-        x: Math.round(
-          Math.random() * window.innerWidth - window.innerWidth / 2
-        ),
-        y: Math.round(Math.random() * maxSize * 3) + window.innerHeight,
+        x: Math.round(Math.random() * window.innerWidth),
+        y:
+          -1 *
+          (origin.y + Math.round(Math.random() * maxSize) + maxSize + blur),
       };
     }
 
+    function randSpeed() {
+      return Math.round(Math.random() * maxSpeed) + minSpeed + 1;
+    }
+
     function randSize(): number {
-      return Math.round(Math.random() * maxSize + minSize);
+      const _size = Math.round(Math.random() * maxSize + minSize);
+      return _size > 0 ? _size : 1;
     }
 
     function randColor(): ColorRGBA {
@@ -94,23 +106,38 @@ const Bubbles = ({
       };
     }
 
-    function randAngle(): number {
-      return Math.round(Math.random() * 90);
+    function calcAnimTime() {
+      if (size === 0) return 10;
+
+      const dx = finalPosition.x - origin.x;
+      const dy = finalPosition.y - origin.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      const sToMs = 1000;
+      // time = distance / speed
+      const time = sToMs * (distance / randSpeed());
+      console.log(time);
+      return time;
     }
 
     const floatAnim = keyframes`
     0% {
-        transform: translateY(0px);
+        opacity: 1;
+        transform: translate(0px, 0px);
     }
+
+    98% {
+        opacity: 1;
+    }
+
     100% {
-        transform: translateY(${-2000 + origin.y}px);
+        opacity: 0;
+        transform: translate(
+          ${finalPosition.x - origin.x}px,
+          ${finalPosition.y}px
+        );
     }
     `;
-
-    // v = d/t
-    const animTime =
-      size === 0 ? 250 : Math.round(Math.random() * 20 + 75) * 1000;
-    console.log(animTime);
 
     return (
       <div
@@ -119,7 +146,7 @@ const Bubbles = ({
         css={css`
           z-index: -5;
           position: fixed;
-          bottom: ${origin.y}px;
+          top: ${origin.y}px;
           left: ${origin.x}px;
 
           width: ${size}px;
@@ -133,8 +160,7 @@ const Bubbles = ({
           );
 
           filter: blur(${blur}px);
-
-          animation: ${floatAnim} ${animTime}ms linear infinite;
+          animation: ${floatAnim} ${calcAnimTime()}ms linear infinite;
         `}
       />
     );
