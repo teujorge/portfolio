8cb772c10db931a67a955fe98735c82ecd8412cc
Matheus <mrljorge@outlook.com>
2023-02-26 22:06:54 -0300

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
 import Head from "next/head";
 import { About } from "@/components/About";
-import { Contact } from "@/components/Contact";
 import { css } from "@emotion/react";
 import { Education } from "@/components/Education";
 import { Experience } from "@/components/Experience";
 import { inView } from "@/utils/inView";
 import { LeftBar } from "@/components/LeftBar";
 import { Projects } from "@/components/Projects";
-import { SolarSystem } from "@/components/SolarSystem";
 import { Title } from "@/components/Title";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect } from "react";
 import { World } from "@/components/World";
+import Bubbles from "@/components/Bubbles";
 /** @jsxImportSource @emotion/react */
 
 export default function Home() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
   // handle in view animation
   useEffect(() => {
     window.scrollTo({ top: 0 });
@@ -59,28 +56,6 @@ export default function Home() {
     };
   }, []);
 
-  // handle scroll position state
-  useEffect(() => {
-    // handle scroll position state
-    function handleScroll() {
-      const scrollDiff = Math.abs(scrollPosition - window.scrollY);
-      if (scrollDiff > 1) setScrollPosition(window.scrollY);
-    }
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrollPosition]);
-
-  // prevent re-render
-  const memoLeftBar = useMemo(LeftBar, []);
-  const memoAbout = useMemo(About, []);
-  const memoExperience = useMemo(Experience, []);
-  const memoProjects = useMemo(Projects, []);
-  const memoEducation = useMemo(Education, []);
-  const memoContact = useMemo(Contact, []);
-
   return (
     <>
       <Head>
@@ -89,6 +64,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Bubbles />
 
       <main
         css={css`
@@ -103,15 +80,13 @@ export default function Home() {
           }
         `}
       >
-        {memoLeftBar}
-        <Title scrollPosition={scrollPosition} />
-        {memoAbout}
-        {memoExperience}
-        {memoProjects}
-        {memoEducation}
+        <LeftBar />
+        <Title />
+        <About />
+        <Experience />
+        <Projects />
+        <Education />
         <World />
-        {/* memoContact */}
-        <SolarSystem scrollPosition={scrollPosition} />
       </main>
     </>
   );
