4d3b04893185644dfcee3bd38b288527e52f680a
Matheus Jorge <mrljorge@outlook.com>
2023-03-06 16:11:10 -0300

    use &apos; to fix build error

--- components/Title.tsx
+++ components/Title.tsx
@@ -51,12 +51,13 @@ export const Title = () => {
         `}
       >
         <p>
-          Whether I'm designing and building fun games or working on complex web
-          apps, I'm constantly pushing myself to develop my software skills and
-          take on new challenges. I believe that software engineering is more
-          than just a job — it's a passion. And I'm excited to share that
-          passion with you through my portfolio. Explore my projects and
-          experience, and let's engineer great things together!
+          Whether I&apos;m designing and building fun games or working on
+          complex web apps, I&apos;m constantly pushing myself to develop my
+          software skills and take on new challenges. I believe that software
+          engineering is more than just a job — it&apos;s a passion. And
+          I&apos;m excited to share that passion with you through my portfolio.
+          Explore my projects and experience, and let&apos;s engineer great
+          things together!
         </p>
       </div>
     </div>
