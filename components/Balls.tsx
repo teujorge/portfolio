fc65075990f797dd85a8cb2696704a157735bc5f
Matheus Jorge <mrljorge@outlook.com>
2023-03-10 02:14:38 -0300

    balls follow mouse and dont collide

--- components/Balls.tsx
+++ components/Balls.tsx
@@ -5,7 +5,12 @@ interface Props {
   numBalls: number;
 }
 
+interface BallBody extends THREE.Mesh {
+  velocity?: THREE.Vector3;
+}
+
 const Balls: React.FC<Props> = ({ numBalls }) => {
+  numBalls = 10;
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -16,30 +21,31 @@ const Balls: React.FC<Props> = ({ numBalls }) => {
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       canvas.clientWidth / canvas.clientHeight,
       0.1,
       1000
     );
+    // const camera = new THREE.OrthographicCamera();
     const renderer = new THREE.WebGLRenderer({ canvas });
 
     renderer.setPixelRatio(pixelRatio);
     renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
-    const balls: THREE.Mesh[] = [];
-
+    // create balls
+    const balls: BallBody[] = [];
     for (let i = 0; i < numBalls; i++) {
-      const geometry = new THREE.SphereGeometry(1, 32, 32);
+      const geometry = new THREE.SphereGeometry(0.1, 32, 32);
       const material = new THREE.MeshStandardMaterial({
         color: new THREE.Color(`hsl(${Math.random() * 360}, 100%, 50%)`),
-        metalness: 0.5,
-        roughness: 0.5,
+        metalness: 0.75,
+        roughness: 0.75,
       });
       const ball = new THREE.Mesh(geometry, material);
       ball.position.set(
         Math.random() * 20 - 10,
         Math.random() * 20 - 10,
-        Math.random() * 20 - 10
+        Math.random() * (20 - 15) + 15
       );
       balls.push(ball);
       scene.add(ball);
@@ -74,12 +80,54 @@ const Balls: React.FC<Props> = ({ numBalls }) => {
     function animate() {
       requestAnimationFrame(animate);
 
-      balls.forEach((ball) => {
-        ball.position.x += (mouse.x * 5 - ball.position.x) * 0.02;
-        ball.position.y += (-mouse.y * 5 - ball.position.y) * 0.02;
-        ball.rotation.x += 0.01;
-        ball.rotation.y += 0.01;
-      });
+      for (let i = 0; i < balls.length; i++) {
+        let cumulativeForce = new THREE.Vector3(0, 0, 0);
+
+        // calculate force from other balls and prevent collisions
+        for (let j = 0; j < balls.length; j++) {
+          // ignore "this" ball
+          if (i === j) continue;
+
+          const distance = balls[i].position.distanceTo(balls[j].position);
+          const radiusSum = 0.25;
+
+          if (distance < radiusSum) {
+            // balls are colliding, adjust their velocities to push them apart
+            const direction = new THREE.Vector3()
+              .subVectors(balls[i].position, balls[j].position)
+              .normalize();
+            const adjustment = direction.multiplyScalar(
+              (radiusSum - distance) / 2
+            );
+
+            balls[i].position.add(adjustment);
+            balls[j].position.sub(adjustment);
+          }
+        }
+
+        // calculate force from mouse
+        const mousePos = new THREE.Vector3(mouse.x, mouse.y, 18.75);
+        const distanceToMouse = balls[i].position.distanceTo(mousePos);
+        const directionToMouse = new THREE.Vector3()
+          .subVectors(mousePos, balls[i].position)
+          .normalize();
+        const mouseForce = directionToMouse
+          .multiplyScalar(1 / (distanceToMouse * distanceToMouse))
+          .clampLength(0, 0.01);
+        cumulativeForce.add(mouseForce);
+
+        // calculate force perpendicular to direction towards mouse
+        const perpendicularDirection = new THREE.Vector3(
+          -directionToMouse.y,
+          directionToMouse.x,
+          0
+        );
+        const perpendicularForce = perpendicularDirection.clampLength(0, 0.001);
+        cumulativeForce.add(perpendicularForce);
+
+        balls[i].velocity = cumulativeForce.clampLength(0.01, 1);
+        balls[i].position.add(balls[i].velocity!);
+      }
 
       renderer.render(scene, camera);
     }
