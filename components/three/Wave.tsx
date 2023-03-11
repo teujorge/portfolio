9770aea59acb546d4f1b8b688bba3de3b4f52d9d
Matheus Jorge <mrljorge@outlook.com>
2023-03-11 18:00:05 -0300

    cleanup

--- components/three/Wave.tsx
+++ components/three/Wave.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { Canvas, Vector3, useFrame } from "@react-three/fiber";
+import { Canvas, RootState, Vector3, useFrame } from "@react-three/fiber";
 
 type Point = {
   x: number;
@@ -19,71 +19,37 @@ const Sphere = ({
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef<any>();
 
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
-
-  const [scale, setScale] = useState(1);
   const [position, setPosition] = useState(pos);
-  const [velocity, setVelocity] = useState({
-    x: 0,
-    y: 1,
-    z: 0,
-  });
 
-  // Subscribe this component to the render-loop, rotate the mesh every frame
+  // subscribe this component to the render-loop
   useFrame((state, delta) => {
-    handleScale(delta);
-    handlePosition(delta);
+    handlePosition(state);
   });
 
-  function handleScale(delta: number) {
-    // scale up
-    if (hovered && scale < 1.5) {
-      setScale(scale + delta);
-    }
-    // scale down
-    else if (scale > 1) {
-      setScale(scale - delta);
-    }
-  }
+  function handlePosition(state: RootState) {
+    // calculate y position based on x and z position using a sine wave
+    const wave = Math.sin(
+      (position.x + position.z) / 10 + state.clock.elapsedTime
+    );
+    const y = minY + ((maxY - minY) * (wave + 1)) / 2;
 
-  function handlePosition(delta: number) {
-    // need to go down
-    if (position.y > maxY) {
-      velocity.y -= delta;
-    }
-
-    // need to go up
-    else if (position.y < minY) {
-      velocity.y += delta;
-    }
-
-    setVelocity({ ...velocity });
-
-    // move
-    position.y += velocity.y * delta;
-    setPosition({ ...position });
+    // update position
+    setPosition({
+      ...position,
+      //   x,
+      y,
+    });
   }
 
   function pointToVector(p: Point): Vector3 {
     return [p.x, p.y, p.z];
   }
 
-  // Return the view, these are regular Three.js elements expressed in JSX
+  // return the view, these are regular Three.js elements expressed in JSX
   return (
-    <mesh
-      ref={ref}
-      position={pointToVector(position)}
-      scale={scale}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
+    <mesh ref={ref} position={pointToVector(position)}>
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial
-        color={hovered ? "hotpink" : "orange"}
-        roughness={0.75}
-        metalness={0.5}
-      />
+      <meshStandardMaterial roughness={0.75} metalness={0.5} />
     </mesh>
   );
 };
@@ -93,14 +59,14 @@ const Wave = () => {
 
   const maxX = 50;
   const maxZ = 50;
-  const spacing = 4;
+  const spacing = 5;
 
   const minY = 0;
   const maxY = 1.2;
 
-  for (let x = 1; x < maxX; x++) {
-    for (let z = 1; z < maxZ; z++) {
-      allPositions.push({ x: spacing * x, y: (x + z) / 10, z: spacing * z });
+  for (let x = 0; x < maxX; x++) {
+    for (let z = 0; z < maxZ; z++) {
+      allPositions.push({ x: spacing * x, y: 0, z: spacing * z });
     }
   }
 
@@ -108,7 +74,7 @@ const Wave = () => {
     <Canvas
       camera={{
         fov: 55,
-        position: [(spacing * maxZ) / 2, 10, 100],
+        position: [(spacing * maxZ) / 2, 50, 200],
       }}
     >
       <ambientLight />
