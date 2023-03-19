70f5ad4b21f36fee8e21bf1ff640152c56553129
Matheus Jorge <mrljorge@outlook.com>
2023-03-18 22:54:38 -0300

    create contact, netlify submit form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,15 +1,77 @@
-import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { FormEvent } from "react";
 
 export const Contact = () => {
   return (
-    <div
-      className="section"
-      css={css`
-        background-color: red;
-      `}
-    >
+    <div className="section">
       <h2>get in touch</h2>
+
+      <div
+        className="reveal"
+        css={css`
+          margin: 20px;
+          padding: 40px;
+
+          border-radius: var(--border-radius);
+          background-color: var(--off-background-color);
+          box-shadow: 0px 0px 8px var(--shadow-color);
+
+          @media (max-width: 700px) {
+            padding: 20px;
+          }
+        `}
+      >
+        <form
+          css={css`
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-content: center;
+
+            label,
+            input,
+            textarea,
+            button {
+              font-family: inherit;
+
+              width: 300px;
+            }
+
+            label {
+              margin-top: 10px;
+            }
+
+            input,
+            textarea {
+              margin-top: 10px;
+              margin-bottom: 10px;
+              padding: 10px;
+            }
+
+            button {
+              margin-top: 20px;
+              padding: 10px;
+            }
+          `}
+          name="portfolio-contact-form"
+          method="POST"
+          data-netlify="true"
+        >
+          <label className="reveal">Name:</label>
+          <input className="reveal" type="text" name="name" />
+
+          <label className="reveal">Email:</label>
+          <input className="reveal" type="email" name="email" />
+
+          <label className="reveal">Message:</label>
+          <textarea className="reveal" name="message" />
+
+          <button className="reveal" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
