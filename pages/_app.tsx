c14872faad8c0586a69275fb458fa86f0c28235a
Matheus <mrljorge@outlook.com>
2023-04-03 12:28:58 -0300

    save window size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { createContext, useEffect, useState } from "react";
 
 export const MOBILE_WIDTH = 1000;
-
+export const windowSize = { width: 0, height: 0 };
 export const AppContext = createContext({ isMobile: false });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,11 +11,14 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // handle mobile v. desktop
   useEffect(() => {
-    const handleResize = () => {
+    function handleResize() {
+      windowSize.width = window.innerWidth;
+      windowSize.height = window.innerHeight;
+
       const _isMobile = window.innerWidth <= MOBILE_WIDTH;
       console.log(_isMobile, isMobile);
       if (isMobile !== _isMobile) setIsMobile(_isMobile);
-    };
+    }
     handleResize(); // set initial state
 
     window.addEventListener("resize", handleResize);
