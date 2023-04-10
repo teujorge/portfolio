0a1f4240a0f8180ecabb9c68810e84ee94b20bcb
Matheus Jorge <mrljorge@outlook.com>
2023-04-10 13:47:22 -0300

    redesign

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -65,90 +65,84 @@ export const Hero = () => {
       className="section"
       css={css`
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
 
+        width: 100%;
         min-height: 100vh;
 
-        @media (max-width: 900px) {
-          flex-direction: column;
+        h1 {
+          font-size: calc(30px + 4vw);
+          font-weight: black;
+          /* color: var(--primary-color); */
+          text-align: left;
+          width: 100%;
         }
-      `}
-    >
-      {/* headers */}
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin: 20px;
 
+        p {
+          font-size: calc(40px + 4vw);
+          margin-top: 10px;
+          margin-bottom: 30px;
+          min-height: 2.5rem;
+          text-align: left;
+          width: 100%;
+        }
+
+        button {
+          margin-top: 20px;
+          width: fit-content;
+        }
+
+        @media (max-width: 1000px) {
           h1 {
-            font-size: calc(30px + 4vw);
-            font-weight: black;
-            color: var(--primary-color);
-          }
-
-          h3 {
-            font-size: calc(18px + 1vw);
-            font-weight: 400;
-            opacity: 0.9;
+            text-align: center;
           }
 
           p {
-            font-size: 1.7rem;
-            margin-top: 10px;
-            margin-bottom: 30px;
-            min-height: 2.5rem;
+            text-align: center;
           }
+        }
+      `}
+    >
+      <p>
+        {currentHeaderText}
+        <span
+          css={css`
+            width: 20px;
+            height: 4px;
+            display: inline-block;
+            border-radius: var(--border-radius);
+            background-color: var(--primary-color);
+            transform: translateY(2px);
+            animation: ${blinkAnimation} 1s infinite;
+          `}
+        />
+      </p>
 
-          button {
-            width: fit-content;
-          }
+      <h1>Matheus Jorge</h1>
 
-          @media (max-width: 900px) {
-            margin: 10px;
+      <div
+        css={css`
+          margin: 80px;
 
-            h1,
-            h3 {
-              text-align: center;
-            }
+          width: 80%;
+          height: 5px;
 
-            p {
-              font-size: 1.25rem;
-            }
-          }
+          border-radius: 50%;
+          background-color: var(--primary-color);
         `}
+      />
+
+      <button
+        onClick={() =>
+          document
+            .getElementById("projects-section")
+            ?.scrollIntoView({ behavior: "smooth" })
+        }
       >
-        <h1>Matheus Jorge</h1>
-
-        <p>
-          {currentHeaderText}
-          <span
-            css={css`
-              width: 20px;
-              height: 4px;
-              display: inline-block;
-              border-radius: var(--border-radius);
-              background-color: var(--primary-color);
-              transform: translateY(2px);
-              animation: ${blinkAnimation} 1s infinite;
-            `}
-          />
-        </p>
-
-        <button
-          onClick={() =>
-            document
-              .getElementById("projects-section")
-              ?.scrollIntoView({ behavior: "smooth" })
-          }
-        >
-          View My Projects
-        </button>
-      </div>
+        View My Projects
+      </button>
     </div>
   );
 };
