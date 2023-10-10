f51d77fe02b90527c0d8afe733a4ab0023287a36
Matheus Jorge <mrljorge@outlook.com>
2023-10-10 12:41:36 -0500

    remove app isMobile state

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { createContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { inView } from "@/utils/inView";
 import BubblesBg from "@/components/BubblesBg";
 import { LeftBar } from "@/components/LeftBar";
 
 export const MOBILE_WIDTH = 1024;
 export const windowSize = { width: 0, height: 0 };
-export const AppContext = createContext({ isMobile: false });
 
 export default function App({ children }: { children: React.ReactNode }) {
-  const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
-    window.scrollTo({ top: 0 });
-
     function handleResize() {
       windowSize.width = window.innerWidth;
       windowSize.height = window.innerHeight;
-
-      const _isMobile = window.innerWidth <= MOBILE_WIDTH;
-      if (isMobile !== _isMobile) setIsMobile(_isMobile);
     }
     handleResize(); // set initial state
 
@@ -41,13 +33,13 @@ export default function App({ children }: { children: React.ReactNode }) {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [isMobile]);
+  }, []);
 
   return (
-    <AppContext.Provider value={{ isMobile: isMobile }}>
+    <>
       <BubblesBg className="-z-10" />
-      <LeftBar />
       {children}
-    </AppContext.Provider>
+      <LeftBar />
+    </>
   );
 }
