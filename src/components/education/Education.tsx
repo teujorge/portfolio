2359ee7e6353ab4df10ead16fbe7e46fb0f9edf6
Matheus Jorge <mrljorge@outlook.com>
2023-10-10 12:42:19 -0500

    overhaul : education

---
+++ src/components/education/Education.tsx
@@ -0,0 +1,65 @@
+import { School } from "./School";
+
+export const Education = () => {
+  return (
+    <div className="subsection">
+      <h3>Education</h3>
+
+      <School
+        school={"University of Vermont"}
+        degree={"BS Mechanical Engineering"}
+        city={"Burlington"}
+        country={"USA"}
+        date={"2016-2020"}
+        description={`
+          The University of Vermont is a public research 
+          university located in Burlington, Vermont, a 
+          beautiful city nestled in the heart of the Green 
+          Mountains. As a BS Mechanical Engineering major with 
+          a Computer Science Minor, I have been part of a 
+          rigorous academic program that emphasized hands-on 
+          learning, innovation, and sustainability. Outside of 
+          the classroom, I worked in the Universities' 
+          Fabrication Laboratory, enjoyed outdoor activities 
+          like skiing and hiking, and explored the food and 
+          culture scene.
+        `}
+      />
+
+      <School
+        school={"American School Foundation"}
+        degree={"High School"}
+        city={"Mexico City"}
+        country={"MX"}
+        date={"2015-2016"}
+        description={`
+          Mexico City is a vibrant metropolis in the heart of 
+          Mexico, with a rich history and culture. Studying 
+          in Mexico City for my last year of high school has 
+          exposed me to a new range of cultural traditions 
+          and amazing foods. I have also had the chance to 
+          practice my Spanish language skills and engage with 
+          the local community.
+        `}
+      />
+
+      <School
+        school={"American Community School"}
+        degree={"High School"}
+        city={"Abu Dhabi"}
+        country={"UAE"}
+        date={"2008-2015"}
+        description={`
+          Abu Dhabi is the capital city of the United Arab 
+          Emirates and a hub of culture, commerce, and 
+          education in the Middle East. Studying in Abu Dhabi 
+          for middle and high school has given me a unique 
+          perspective on this dynamic region, with opportunities 
+          to learn about Arabic language and culture, engage 
+          with a diverse student body, and explore amazing 
+          landmarks.
+        `}
+      />
+    </div>
+  );
+};
---
+++ src/components/education/School.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { toggleExpansion } from "@/utils/expansion";
+
+export const School = ({
+  school,
+  degree,
+  city,
+  country,
+  date,
+  description,
+}: {
+  school: string;
+  degree: string;
+  city: string;
+  country: string;
+  date: string;
+  description: string;
+}) => {
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const descriptionContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const descElem = descriptionRef.current!;
+    const descContainerElem = descriptionContainerRef.current!;
+
+    gsap.set(descElem, { height: 0, opacity: 0 });
+
+    function toggleDescription() {
+      toggleExpansion(descriptionRef);
+    }
+
+    descContainerElem.addEventListener("mouseenter", toggleDescription);
+    descContainerElem.addEventListener("mouseleave", toggleDescription);
+
+    return () => {
+      descContainerElem.removeEventListener("mouseenter", toggleDescription);
+      descContainerElem.removeEventListener("mouseleave", toggleDescription);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={descriptionContainerRef}
+      className="reveal flex flex-row items-center justify-center border-[var(--primary-color)] md:m-6 md:p-4 md:border-r-2"
+    >
+      <div className="flex flex-col justify-center items-start my-2.5 md:mx-7.5">
+        <h4 className="reveal">
+          {degree}, {school}, {city}, {country}
+        </h4>
+        <p className="reveal text-xs">{date ? date : "current"}</p>
+        <p ref={descriptionRef} className="md:reveal m-2.5">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
