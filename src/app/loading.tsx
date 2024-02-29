b9ec8635a783ff13e80148367c8e3021da333d35
Matheus Jorge <mrljorge@outlook.com>
2024-02-29 12:46:40 -0300

    add : global loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function GlobalLoading() {
+  return (
+    <main>
+      <div className="section flex flex-col justify-center items-center w-full min-h-screen">
+        <h1 className="animate-pulse">Matheus Jorge</h1>
+      </div>
+    </main>
+  );
+}
