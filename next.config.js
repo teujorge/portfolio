a64bc394806b3acba3d6c61bfc9c0889560508a7
Matheus Jorge <mrljorge@outlook.com>
2024-03-20 18:40:30 -0300

    optimization : 700+kb to 150kb first load bundle - lazy load spline

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withBundleAnalyzer(nextConfig);
