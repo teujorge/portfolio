c4433b4c9e4d887256ee31b08633d1353b57cf4f
Matheus Jorge <mrljorge@outlook.com>
2024-03-20 18:45:37 -0300

    fix : add priority to hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ export const Hero = () => {
   return (
     <div className="section w-full min-h-screen flex flex-col lg:flex-row items-center justify-center">
       <Image
+        priority
         src={HeroImage}
         alt="Matheus Jorge"
         className="max-w-[80%] sm:max-w-[60%] lg:max-w-[30%]"
